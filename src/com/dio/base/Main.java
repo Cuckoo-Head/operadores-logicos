868a63cba22e5eae07c028c21c612b46b32c2bc4
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-13 15:16:40 -0300

    operadores logicos adicionados

--- src/com/dio/base/Main.java
+++ src/com/dio/base/Main.java
@@ -4,7 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		boolean t1 = true;
+		boolean f1 = false;
+		boolean t2 = true;
+		boolean f2 = false;
 		
-	}
+		System.out.println("The &&(and) operator:");
+		System.out.println("True and true = " + (t1 && t2));
+		System.out.println("True and false = " + (t1 && f1));
+		System.out.println("False and false = " + (f1 && f2));
+		System.out.println();
+		
+		System.out.println("The ||(or) operator:");
+		System.out.println("True and true = " + (t1 || t2));
+		System.out.println("True and false = " + (t1 || f1));
+		System.out.println("False and false = " + (f1 || f2));
+		System.out.println();
+		
+		System.out.println("The ^(xor) operator:");
+		System.out.println("True and true = " + (t1 ^ t2));
+		System.out.println("True and false = " + (t1 ^ f1));
+		System.out.println("False and false = " + (f1 ^ f2));
+		System.out.println();
+		
+		System.out.println("The ! operator:");
+		System.out.println("True and ! = " + !t1);
+		System.out.println("False and ! = " + !f1);
+		System.out.println();
+		}
 
 }
